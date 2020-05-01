9d93bf934edecc4d7e15be85f04d593fdf3344e8
Aldo Díaz <aldodiaz64@gmail.com>
2020-05-01 17:22:03 -0300

    v0.1

--- RotationToolbox/q2R.m
+++ RotationToolbox/q2R.m
@@ -3,14 +3,12 @@ function R = q2R(q)
 %
 % Aldo Diaz, University of Campinas, 2019
 
-q1 = q(1);   q2 = q(2);   q3 = q(3);   q4 = q(4);
+R = [ q(1)^2+q(2)^2-q(3)^2-q(4)^2  2*(q(2)*q(3)+q(1)*q(4))      2*(q(2)*q(4)-q(1)*q(3));
+      2*(q(2)*q(3)-q(1)*q(4))      q(1)^2-q(2)^2+q(3)^2-q(4)^2  2*(q(3)*q(4)+q(1)*q(2));
+      2*(q(2)*q(4)+q(1)*q(3))      2*(q(3)*q(4)-q(1)*q(2))      q(1)^2-q(2)^2-q(3)^2+q(4)^2 ];
 
-R = [ q1^2+q2^2-q3^2-q4^2	2*(q2*q3+q1*q4)         2*(q2*q4-q1*q3);
-        2*(q2*q3-q1*q4)     q1^2-q2^2+q3^2-q4^2     2*(q3*q4+q1*q2);
-        2*(q2*q4+q1*q3)     2*(q3*q4-q1*q2)         q1^2-q2^2-q3^2+q4^2 ];
-
-if norm(q) ~= 1 % if q is a non-unit quaternion
-    R = (1/(q1^2 + q2^2 + q3^2+ q4^2)).*R;
+if norm(q) ~= 1 % 'q' is a non-unit quaternion
+    R = (1/(q(1)^2+q(2)^2+q(3)^2+q(4)^2)).*R;
 end
 
 end
---
+++ RotationToolbox/qConj.m
@@ -0,0 +1,8 @@
+function q = qConj(q)
+%% Function to calculate the quaternion conjugate
+%
+% Aldo Diaz, University of Campinas, 2020
+
+q = [ q(1); -q(2); -q(3); -q(4) ];
+
+end
