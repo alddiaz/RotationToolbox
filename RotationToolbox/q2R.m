a6ab18b0ae4e4dc480507990faffeaa77bfc7070
Aldo Díaz <aldodiaz64@gmail.com>
2020-05-07 14:03:24 -0300

    v1.0

--- RotationToolbox/q2R.m
+++ RotationToolbox/q2R.m
@@ -3,12 +3,16 @@ function R = q2R(q)
 %
 % Aldo Diaz, University of Campinas, 2020
 
-R = [ q(1)^2+q(2)^2-q(3)^2-q(4)^2  2*(q(2)*q(3)+q(1)*q(4))      2*(q(2)*q(4)-q(1)*q(3));
-      2*(q(2)*q(3)-q(1)*q(4))      q(1)^2-q(2)^2+q(3)^2-q(4)^2  2*(q(3)*q(4)+q(1)*q(2));
-      2*(q(2)*q(4)+q(1)*q(3))      2*(q(3)*q(4)-q(1)*q(2))      q(1)^2-q(2)^2-q(3)^2+q(4)^2 ];
+norm_square = q(1)^2+q(2)^2+q(3)^2+q(4)^2; % square of the norm
 
-if norm(q) ~= 1 % 'q' is a non-unit quaternion
-    R = (1/(q(1)^2+q(2)^2+q(3)^2+q(4)^2)).*R;
-end
+if norm_square ~= 0 % 'q' is non-zero quaternion
+    R = [ q(1)^2+q(2)^2-q(3)^2-q(4)^2  2*(q(2)*q(3)+q(1)*q(4))      2*(q(2)*q(4)-q(1)*q(3));
+          2*(q(2)*q(3)-q(1)*q(4))      q(1)^2-q(2)^2+q(3)^2-q(4)^2  2*(q(3)*q(4)+q(1)*q(2));
+          2*(q(2)*q(4)+q(1)*q(3))      2*(q(3)*q(4)-q(1)*q(2))      q(1)^2-q(2)^2-q(3)^2+q(4)^2 ];
 
+    if norm_square ~= 1 % 'q' is non-unit quaternion
+        R = R/norm_square;
+    end
+else
+    R = eye(3);
 end
